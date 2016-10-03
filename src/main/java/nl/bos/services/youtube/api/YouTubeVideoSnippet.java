6b220c189d0c431476d35037ff33c0f6c114aaaf
Antal Bos <Antal.Bos@klm.com>
2016-10-03 12:20:15 +0200

    Simple challenge video 7

--- src/main/java/nl/bos/services/youtube/api/YouTubeVideoSnippet.java
+++ src/main/java/nl/bos/services/youtube/api/YouTubeVideoSnippet.java
@@ -1,7 +1,7 @@
 package nl.bos.services.youtube.api;
 
 public class YouTubeVideoSnippet {
-	private String title, description;
+	private String title, description, channelId, publishedAt, channelTitle;
 
 	public String getTitle() {
 		return title;
@@ -17,5 +17,29 @@ public class YouTubeVideoSnippet {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public String getChannelId() {
+		return channelId;
+	}
+
+	public void setChannelId(String channelId) {
+		this.channelId = channelId;
+	}
+
+	public String getPublishedAt() {
+		return publishedAt;
+	}
+
+	public void setPublishedAt(String publishedAt) {
+		this.publishedAt = publishedAt;
+	}
+
+	public String getChannelTitle() {
+		return channelTitle;
+	}
+
+	public void setChannelTitle(String channelTitle) {
+		this.channelTitle = channelTitle;
 	}
 }
